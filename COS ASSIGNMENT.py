31dffbedc985632b01bbc5ad77c08098b9af5845
itzthedking <itzthedking@gmail.com>
2024-12-04 15:22:49 -0800

    Add files via upload

---
+++ COS ASSIGNMENT.py
@@ -0,0 +1,14 @@
+yoruba_dictionary = {
+    'ola' :  'wealth',
+    'oju' : 'eye',
+    'ife' : 'love',
+    'ekaaro' : 'Good morning',
+    'omo' : 'child',
+    'ojo' : 'day',
+    'ore' : 'friend',
+    'iya' : 'mother',
+    'ewa' : 'beans',
+}
+
+word = input("enter yoruba word: ")
+print(yoruba_dictionary[word])
